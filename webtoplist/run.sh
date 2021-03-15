92acc7af08a1d5993cb4a28330fc0259b2b3f119
zhansu <zhansu@tencent.com>
2021-03-15 20:14:56 +0800

    debug

--- webtoplist/run.sh
+++ webtoplist/run.sh
@@ -2,12 +2,12 @@
 ###
  # @Author: Zhan Su
  # @Date: 2021-03-03 11:20:16
- # @LastEditTime: 2021-03-03 11:42:09
+ # @LastEditTime: 2021-03-15 20:14:38
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
 ### 
 for line in $(cat countryCode)
 do
     printPer $line
-    java TopSites AKIATNXZERZXGKH76H4F BtWf/+CCEboCjZL3AipSRykG4XS1YJVa/FxtZVdw $line
+    java TopSites * * $line
 done
