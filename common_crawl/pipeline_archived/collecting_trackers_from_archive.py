1d8c0e9f6260d4363c4b0ca1887b0f1480ce65e9
ZhanSu <zhan.su@di.ku.dk>
2024-04-10 16:24:20 +0200

    wip

--- common_crawl/pipeline_archived/collecting_trackers_from_archive.py
+++ common_crawl/pipeline_archived/collecting_trackers_from_archive.py
@@ -18,68 +18,8 @@ from warc_module.warc_utils import (
     get_outer_link,
 )
 from utils import extract_trackers_from_internet_archive
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--output_path",
-    type=str,
-    default="debug/IA/GOV/",
-    help="output path",
-)
-parser.add_argument(
-    "--input_path",
-    type=str,
-    default="websci/scanning_websites/government_websites_page_rank_2023_top_500_historical_url.csv",
-    help="input path",
-)
-parser.add_argument(
-    "--year",
-    type=str,
-    default="2014",
-    help="year",
-)
-
-parser.add_argument(
-    "--skiprows",
-    type=int,
-    default=0,
-    help=0,
-)
-parser.add_argument(
-    "--unit_test",
-    action="store_true",
-    help="unit test",
-)
-
-parser.add_argument(
-    "--wandb",
-    action="store_true",
-    help="wandb",
-)
-
-parser.add_argument(
-    "--sleep_second",
-    type=int,
-    default=5,
-    help="sleep_second",
-)
-
-parser.add_argument(
-    "--multi_process",
-    action="store_true",
-    help="multi process",
-)
-
-parser.add_argument(
-    "--num_process",
-    type=int,
-    default=1,
-    help="number of processes",
-)
-
-args = parser.parse_args()
-
+from pipeline_archived.archive_modules import collect_trackers_from_map_ia
+from pipeline_archived.archive_modules import args
 
 ###### add log ######
 logging.basicConfig(
@@ -166,40 +106,6 @@ t = time.time()
 
 #################################### collecting from historical trackers from Internet Archive ###############################
 
-fout = open(args.output_path + f"/archived_websites_{args.year}.json", "w")
-
-
-def collect_trackers_from_map_ia(row):
-    try:
-        hostname = row["hostname"]
-        history_url = row["url"]
-        # get historical url
-
-        if history_url in ["NAN", "DEAD"]:
-            return hostname + "\t" + "EMPTY_URL" + "\n"
-        fields = history_url.split("/")
-        fields[4] = fields[4] + "id_"
-        history_url = "/".join(fields)
-        time.sleep(args.sleep_second)
-        logger.info(f"collecting number:{hostname}")
-        trackers = extract_trackers_from_internet_archive(
-            history_url, get_text_selectolax, if_wandb=args.wandb
-        )
-        if trackers == []:
-            fout.write(
-                json.dumps({"hostname": hostname, "trackers": "NO_TRACKERS"}) + "\n"
-            )
-        elif trackers == "REFUSED":
-            fout.write(json.dumps({"hostname": hostname, "trackers": "REFUSED"}) + "\n")
-        elif trackers == "DEAD":
-            fout.write(json.dumps({"hostname": hostname, "trackers": "DEAD"}) + "\n")
-        else:
-            fout.write(json.dumps({"hostname": hostname, "trackers": trackers}) + "\n")
-        fout.flush()
-    except Exception as e:
-        # print(history_url)
-        print(e)
-
 
 # only test one historical snapshot
 def test_archive():
@@ -224,13 +130,12 @@ def collecting_single_thread():
         hostname = item["hostname"]
         history_url = item["url"]
 
-        fields = history_url.split("/")
-        fields[4] = fields[4] + "id_"
-        history_url = "/".join(fields)
-
         if history_url in ["NAN", "DEAD"]:
             fout.write(hostname + "\t" + "EMPTY_URL" + "\n")
             continue
+        fields = history_url.split("/")
+        fields[4] = fields[4] + "id_"
+        history_url = "/".join(fields)
         time.sleep(args.sleep_second)
         logger.info(f"collecting number:{e}:{hostname}")
         trackers = extract_trackers_from_internet_archive(
