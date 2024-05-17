d001fe691814676aec9cc5624ec5a013a222a9a9
shuishen112 <549810718@qq.com>
2024-05-17 11:24:42 +0200

    add multi-thread for get_country info

--- common_crawl/pipeline_scan/get_country_from_domain.py
+++ common_crawl/pipeline_scan/get_country_from_domain.py
@@ -2,18 +2,32 @@ import whois
 import pandas as pd
 import argparse
 import json
+import tqdm
+import multiprocessing as mp
 
 parser = argparse.ArgumentParser(description="Get country from domain")
 
 parser.add_argument("--input_path", type=str, help="domain name")
+parser.add_argument("--output_path", type=str, help="output path")
+parser.add_argument(
+    "--multi_process",
+    action="store_true",
+    help="multi process collecting",
+)
+parser.add_argument(
+    "--num_process",
+    type=int,
+    default=10,
+    help="number of process",
+)
 
 args = parser.parse_args()
 
-df = pd.read_csv(args.input_path, names=["domain", "count"], sep="\t")
 
-if __name__ == "__main__":
-    fout = open("pipeline_scan/domain_country.json", "w")
-    for i, row in df.iterrows():
+def single_process():
+    df = pd.read_csv(args.input_path, names=["domain", "count"], sep="\t")
+    fout = open(args.output_path, "w")
+    for i, row in tqdm.tqdm(df.iterrows()):
         domain = row["domain"].strip()
         try:
             json_format = whois.whois(domain)
@@ -30,3 +44,38 @@ if __name__ == "__main__":
         except Exception as e:
             print(e)
             print("error", domain)
+
+
+df = pd.read_csv(args.input_path, names=["domain", "count"], sep="\t")
+
+if __name__ == "__main__":
+
+    def get_country_from_whois(row):
+        try:
+            domain = row["domain"].strip()
+            json_format = whois.whois(domain)
+            return (
+                json.dumps(
+                    {"domain": domain, "info": json_format},
+                    indent=4,
+                    sort_keys=True,
+                    default=str,
+                )
+                + "\n"
+            )
+        except Exception as e:
+            print(e)
+            print("error", domain)
+            return None
+
+    if args.multi_process:
+        fout = open(args.output_path, "w")
+        pool = mp.Pool(args.num_process)
+        v = json.loads(df.to_json(orient="records"))
+        for i, result in enumerate(
+            tqdm(pool.imap_unordered(get_country_from_whois, v), total=len(df))
+        ):
+            fout.write(result)
+            fout.flush()
+    else:
+        single_process()
